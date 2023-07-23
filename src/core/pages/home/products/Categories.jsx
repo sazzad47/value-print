186c8444a031966224ba25e7f956f1803c2fb836
Sazzad <sazzad47.ju@gmail.com>
2023-07-23 18:55:28 +0600

    make it better

--- src/core/pages/home/products/Categories.jsx
+++ src/core/pages/home/products/Categories.jsx
@@ -110,7 +110,7 @@ function TabPanel({ value, products, categories }) {
  
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-[3rem] bg-white">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-[3rem] bg-white">
         {filteredProducts.map((product, index) => (
           <div
             key={index}
