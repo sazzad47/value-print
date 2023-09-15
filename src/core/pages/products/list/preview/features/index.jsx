01109d24418139693a0f638f58296a8bcacb15f8
Sazzad <sazzad47.ju@gmail.com>
2023-09-15 11:44:48 +0600

    make it better

--- src/core/pages/products/list/preview/features/index.jsx
+++ src/core/pages/products/list/preview/features/index.jsx
@@ -1,125 +1,165 @@
-import React, { useState, useEffect } from 'react';
-import GeneralPlaceholer from '../../../../../components/GeneralPlaceholer';
-import GeneralPrice from '../generalPrice';
-import { Box, Grid, Paper } from '@mui/material';
-import Dropdown from '../generalFeatures/Dropdown';
+import React, { useState, useEffect } from "react";
+import GeneralPlaceholer from "../../../../../components/GeneralPlaceholer";
+import GeneralPrice from "../generalPrice";
+import { Box, Grid, Paper } from "@mui/material";
+import Dropdown from "../generalFeatures/Dropdown";
 
-function PricingOptions({
-  data,
-  featuresState,
-  setFeaturesState,
-}) {
+function PricingOptions({ data, featuresState, setFeaturesState }) {
   const [selectedTable, setSelectedTable] = useState(data[0]);
   const [selectedOptions, setSelectedOptions] = useState({});
   const [selectedPricing, setSelectedPricing] = useState(null);
   const [nextOptions, setNextOptions] = useState({});
-  console.log('data', data)
+
+  const [hiddenColumns, setHiddenColumns] = useState([]);
+  console.log("nextOptions", nextOptions);
 
   useEffect(() => {
     const initialSelectedOptions = {};
     const initialNextOptions = {};
-  
+
     data[0].columns.forEach((columnName, columnIndex) => {
       const uniqueOptions = new Set();
       const photoMap = {};
-  
+
       data.forEach((table) => {
         table.rows.forEach((row) => {
           if (table.columns.includes(columnName)) {
-            row.cellData[table.columns.indexOf(columnName)].forEach((option) => {
-              const { value, photo, is_popular } = option;
-  
-              if (!uniqueOptions.has(value)) {
-                uniqueOptions.add(value);
-                photoMap[value] = { photo: photo || '', is_popular: is_popular || false };
+            row.cellData[table.columns.indexOf(columnName)].forEach(
+              (option) => {
+                const { value, photo, is_popular } = option;
+
+                if (!uniqueOptions.has(value)) {
+                  uniqueOptions.add(value);
+                  photoMap[value] = {
+                    photo: photo || "",
+                    is_popular: is_popular || false,
+                  };
+                }
               }
-            });
+            );
           }
         });
       });
-  
-      initialSelectedOptions[columnName] = '';
-  
+
+      initialSelectedOptions[columnName] = "";
+
       // Initialize nextOptions only for the first column
       if (columnIndex === 0) {
-        initialNextOptions[columnName] = Array.from(uniqueOptions).map((value) => ({
-          value,
-          photo: photoMap[value].photo,
-          is_popular: photoMap[value].is_popular,
-        }));
+        initialNextOptions[columnName] = Array.from(uniqueOptions).map(
+          (value) => ({
+            value,
+            photo: photoMap[value].photo,
+            is_popular: photoMap[value].is_popular,
+          })
+        );
       } else {
         initialNextOptions[columnName] = [];
       }
     });
-  
+
     setSelectedOptions(initialSelectedOptions);
     setNextOptions(initialNextOptions);
   }, [data]);
-  
+
+  // Use useEffect to remove hidden columns from featureState
+  useEffect(() => {
+    const updatedFeaturesState = { ...featuresState };
+
+    hiddenColumns.forEach((hiddenColumnIndex) => {
+      const hiddenColumnName = selectedTable.columns[hiddenColumnIndex];
+      delete updatedFeaturesState[hiddenColumnName];
+    });
+
+    setFeaturesState(updatedFeaturesState);
+    // eslint-disable-next-line 
+  }, [hiddenColumns, setFeaturesState, selectedTable.columns]);
 
   const handleTableChange = (value, columnName) => {
-
-    console.log('tableName', value)
     const selectedTableName = value;
-    const selectedTableData = data.find((table) => table.tableName === selectedTableName);
-    console.log('selectedTableData', selectedTableData)
+    const selectedTableData = data.find(
+      (table) => table.tableName === selectedTableName
+    );
+
     setSelectedTable(selectedTableData);
 
     const initialSelectedOptions = {
-      [data[0].columns[0]]: { value: selectedTableName, photo: '', is_popular: false },
+      [data[0].columns[0]]: {
+        value: selectedTableName,
+        photo: "",
+        is_popular: false,
+      },
     };
     setSelectedOptions(initialSelectedOptions);
 
     const nextColumnName = selectedTableData.columns[1];
     const nextColumnOptions = new Set();
 
-  
- // Flag to track whether we've reached the current column
- let reachedCurrentColumn = false;
+    // Flag to track whether we've reached the current column
+    let reachedCurrentColumn = false;
 
- selectedTableData.columns.forEach((col) => {
-   if (reachedCurrentColumn) {
-     // Set featureState and nextOptions to null for subsequent columns
-     setFeaturesState((prevFeaturesState) => ({
-       ...prevFeaturesState,
-       [col]: null,
-     }));
-    
-   }
+    selectedTableData.columns.forEach((col) => {
+      if (reachedCurrentColumn) {
+        // Set featureState and nextOptions to null for subsequent columns
+        setFeaturesState((prevFeaturesState) => ({
+          ...prevFeaturesState,
+          [col]: null,
+        }));
+      }
 
-   // Check if the current column matches the columnName
-   if (col === columnName) {
-     reachedCurrentColumn = true;
-   }
- });
+      // Check if the current column matches the columnName
+      if (col === columnName) {
+        reachedCurrentColumn = true;
+      }
+    });
 
- // Flag to track whether we've reached the next column
- let reachedNextColumn = false;
+    // Flag to track whether we've reached the next column
+    let reachedNextColumn = false;
 
- selectedTableData.columns.forEach((col) => {
-   if (reachedNextColumn) {
-    setNextOptions((prevNextOptions) => ({
-      ...prevNextOptions,
-      [col]: [],
-    }));
-    
-   }
+    selectedTableData.columns.forEach((col) => {
+      if (reachedNextColumn) {
+        setNextOptions((prevNextOptions) => ({
+          ...prevNextOptions,
+          [col]: [],
+        }));
+      }
 
-   // Check if the current column matches the nextColumnName
-   if (col === nextColumnName) {
-    reachedNextColumn = true;
-   }
- });
+      // Check if the current column matches the nextColumnName
+      if (col === nextColumnName) {
+        reachedNextColumn = true;
+      }
+    });
+
+    let isCurrentColumnEmpty = true;
 
     selectedTableData.rows.forEach((row) => {
       row.cellData[1].forEach((option) => {
+        if (option.value !== "") {
+          isCurrentColumnEmpty = false; // Set to false if a non-empty option is found
+        }
+      });
+    });
+
+    // Conditionally update hiddenColumns
+    setHiddenColumns((prevHiddenColumns) => {
+      // Filter out columnIndex + 1 if isCurrentColumnEmpty is false
+      return isCurrentColumnEmpty
+        ? [...prevHiddenColumns, 1]
+        : prevHiddenColumns.filter((colIndex) => colIndex !== 1);
+    });
+
+    selectedTableData.rows.forEach((row) => {
+      row.cellData[isCurrentColumnEmpty ? 2 : 1].forEach((option) => {
         const optionToAdd = {
-          value: option.value || '',
-          photo: option.photo || '',
+          value: option.value || "",
+          photo: option.photo || "",
           is_popular: option.is_popular || false,
         };
 
-        if (![...nextColumnOptions].some((existingOption) => existingOption.value === optionToAdd.value)) {
+        if (
+          ![...nextColumnOptions].some(
+            (existingOption) => existingOption.value === optionToAdd.value
+          )
+        ) {
           nextColumnOptions.add(optionToAdd);
         }
       });
@@ -128,14 +168,13 @@ function PricingOptions({
         ...prevFeaturesState,
         [columnName]: value,
       }));
-
     });
 
     setNextOptions((prevNextOptions) => ({
       ...prevNextOptions,
       [nextColumnName]: Array.from(nextColumnOptions).map((value) => ({
         ...value,
-        photo: value.photo || '',
+        photo: value.photo || "",
         is_popular: value.is_popular || false,
       })),
     }));
@@ -151,6 +190,7 @@ function PricingOptions({
     }));
 
     let reachedCurrentColumn = false;
+    let reachedNextColumn = false;
 
     selectedTable.columns.forEach((col) => {
       if (reachedCurrentColumn) {
@@ -160,15 +200,7 @@ function PricingOptions({
           [col]: null,
         }));
       }
-  
-      // Check if the current column matches the columnName
-      if (col === columnName) {
-        reachedCurrentColumn = true;
-      }
     });
-
-    
-
 
     setFeaturesState((prevFeaturesState) => ({
       ...prevFeaturesState,
@@ -176,59 +208,117 @@ function PricingOptions({
     }));
 
     const columnIndex = selectedTable.columns.indexOf(columnName);
+    let nextColumnIndex = columnIndex + 1;
+    let nextColumnName = selectedTable.columns[nextColumnIndex];
+    let isCurrentColumnEmpty = true; // Flag to track if the current column is empty
 
     if (columnIndex < selectedTable.columns.length - 1) {
-      const nextColumnName = selectedTable.columns[columnIndex + 1];
-      const nextColumnOptions = new Set();
+      let nextColumnOptions = new Set();
 
       selectedTable.rows
-        .filter((row) =>
-          selectedTable.columns
-            .slice(0, columnIndex)
-            .every((prevColumnName) =>
-              row.cellData[selectedTable.columns.indexOf(prevColumnName)].some(
-                (option) => selectedOptions[prevColumnName]?.value === option.value
+        .filter(
+          (row) =>
+            selectedTable.columns
+              .slice(0, columnIndex)
+              .filter(
+                (prevColumnName) =>
+                  !hiddenColumns.includes(
+                    selectedTable.columns.indexOf(prevColumnName)
+                  )
               )
-            ) &&
-          row.cellData[columnIndex].some((option) => option.value === value)
+              .every((prevColumnName) =>
+                row.cellData[
+                  selectedTable.columns.indexOf(prevColumnName)
+                ].some(
+                  (option) =>
+                    selectedOptions[prevColumnName]?.value === option.value
+                )
+              ) &&
+            row.cellData[columnIndex].some((option) => option.value === value)
         )
         .forEach((row) => {
-          row.cellData[columnIndex + 1].forEach((option) => {
+          row.cellData[nextColumnIndex].forEach((option) => {
+            // Check if there are non-empty options for this column
+            if (option.value !== "") {
+              isCurrentColumnEmpty = false; // Set to false if a non-empty option is found
+            }
+          });
+        });
+
+      // If isCurrentColumnEmpty is true, move to the next column
+      if (isCurrentColumnEmpty) {
+        nextColumnIndex = selectedTable.columns.indexOf(columnName) + 2;
+        nextColumnName = selectedTable.columns[nextColumnIndex];
+      }
+
+      selectedTable.rows
+        .filter(
+          (row) =>
+            selectedTable.columns
+              .slice(0, columnIndex)
+              .filter(
+                (prevColumnName) =>
+                  !hiddenColumns.includes(
+                    selectedTable.columns.indexOf(prevColumnName)
+                  )
+              )
+              .every((prevColumnName) =>
+                row.cellData[
+                  selectedTable.columns.indexOf(prevColumnName)
+                ].some(
+                  (option) =>
+                    selectedOptions[prevColumnName]?.value === option.value
+                )
+              ) &&
+            row.cellData[columnIndex].some((option) => option.value === value)
+        )
+        .forEach((row) => {
+          row.cellData[nextColumnIndex].forEach((option) => {
             const optionToAdd = {
-              value: option.value || '',
-              photo: option.photo || '',
+              value: option.value || "",
+              photo: option.photo || "",
               is_popular: option.is_popular || false,
             };
 
-            if (![...nextColumnOptions].some((existingOption) => existingOption.value === optionToAdd.value)) {
+            if (
+              ![...nextColumnOptions].some(
+                (existingOption) => existingOption.value === optionToAdd.value
+              )
+            ) {
               nextColumnOptions.add(optionToAdd);
             }
           });
         });
 
-      // Flag to track whether we've reached the next column
- let reachedNextColumn = false;
+      // Conditionally update hiddenColumns
+      setHiddenColumns((prevHiddenColumns) => {
+        // Filter out columnIndex + 1 if isCurrentColumnEmpty is false
+        return isCurrentColumnEmpty
+          ? [...prevHiddenColumns, columnIndex + 1]
+          : prevHiddenColumns.filter(
+              (colIndex) => colIndex !== columnIndex + 1
+            );
+      });
 
- selectedTable.columns.forEach((col) => {
-   if (reachedNextColumn) {
-    setNextOptions((prevNextOptions) => ({
-      ...prevNextOptions,
-      [col]: [],
-    }));
-    
-   }
+      selectedTable.columns.forEach((col) => {
+        if (reachedNextColumn) {
+          setNextOptions((prevNextOptions) => ({
+            ...prevNextOptions,
+            [col]: [],
+          }));
+        }
 
-   // Check if the current column matches the nextColumnName
-   if (col === nextColumnName) {
-    reachedNextColumn = true;
-   }
- });
+        // Check if the current column matches the nextColumnName
+        if (col === nextColumnName) {
+          reachedNextColumn = true;
+        }
+      });
 
       setNextOptions((prevNextOptions) => ({
         ...prevNextOptions,
         [nextColumnName]: Array.from(nextColumnOptions).map((value) => ({
           ...value,
-          photo: value.photo || '',
+          photo: value.photo || "",
           is_popular: value.is_popular || false,
         })),
       }));
@@ -236,8 +326,8 @@ function PricingOptions({
       setSelectedOptions((prevSelectedOptions) => ({
         ...prevSelectedOptions,
         [nextColumnName]: {
-          value: '',
-          photo: '',
+          value: "",
+          photo: "",
           is_popular: false,
         },
       }));
@@ -247,14 +337,21 @@ function PricingOptions({
   useEffect(() => {
     const calculatePrice = () => {
       if (selectedTable && selectedTable.columns && selectedOptions) {
-        if (selectedTable.columns.every((columnName) => selectedOptions[columnName]?.value)) {
+        if (
+          selectedTable.columns.every(
+            (columnName) => selectedOptions[columnName]?.value
+          )
+        ) {
           const matchingRow = selectedTable.rows.find((row) =>
             selectedTable.columns.every((columnName) => {
               const selectedValue = selectedOptions[columnName]?.value;
-              const cellDataForColumn = row.cellData[selectedTable.columns.indexOf(columnName)];
+              const cellDataForColumn =
+                row.cellData[selectedTable.columns.indexOf(columnName)];
               return (
                 cellDataForColumn &&
-                cellDataForColumn.some((option) => option && option.value === selectedValue)
+                cellDataForColumn.some(
+                  (option) => option && option.value === selectedValue
+                )
               );
             })
           );
@@ -279,36 +376,42 @@ function PricingOptions({
           width: "100%",
           height: "auto",
           backgroundColor: "white",
-          position: 'relative',
+          position: "relative",
         }}
       >
-        <GeneralPlaceholer className="optionHeading" text="1. Choose your options" />
+        <GeneralPlaceholer
+          className="optionHeading"
+          text="1. Choose your options"
+        />
         <div className="px-[20px] py-[20px]">
           <Grid className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
-            {selectedTable?.columns.map((columnName, colIdx) => (
-              <React.Fragment key={colIdx}>
-                <Grid
-                  id={`options-container-${colIdx}`}
-                  className="w-full flex flex-col gap-3"
-                >
-                  <h2 className="text-fuchsia-900 font-bold text-lg">
-                    {columnName}
-                  </h2>
-                  
-                      <Dropdown
-                        colIdx={colIdx}
-                        handleChange={handleChange}
-                        handleTableChange={handleTableChange}
-                        nextOptions={nextOptions}
-                        columnName={columnName}
-                        featuresState={featuresState}
-                        setFeaturesState={setFeaturesState}
-                      />
-              
-                </Grid>
-              </React.Fragment>
-            ))}
-            
+            {selectedTable?.columns.map((columnName, colIdx) => {
+              if (hiddenColumns.includes(colIdx)) {
+                return null; // Skip rendering the column
+              }
+
+              return (
+                <React.Fragment key={colIdx}>
+                  <Grid
+                    id={`options-container-${colIdx}`}
+                    className="w-full flex flex-col gap-3"
+                  >
+                    <h2 className="text-fuchsia-900 font-bold text-lg">
+                      {columnName}
+                    </h2>
+                    <Dropdown
+                      colIdx={colIdx}
+                      handleChange={handleChange}
+                      handleTableChange={handleTableChange}
+                      nextOptions={nextOptions}
+                      columnName={columnName}
+                      featuresState={featuresState}
+                      setFeaturesState={setFeaturesState}
+                    />
+                  </Grid>
+                </React.Fragment>
+              );
+            })}
           </Grid>
         </div>
       </Paper>
@@ -319,10 +422,13 @@ function PricingOptions({
             width: "100%",
             height: "auto",
             backgroundColor: "white",
-            position: 'relative',
+            position: "relative",
           }}
         >
-          <GeneralPlaceholer className="quantityHeading" text="2. Choose Quantity & Delivery Speed" />
+          <GeneralPlaceholer
+            className="quantityHeading"
+            text="2. Choose Quantity & Delivery Speed"
+          />
           <div className="px-[20px] py-[20px]">
             <Grid container className="w-full">
               <GeneralPrice
